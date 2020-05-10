dacece5f5ab2b744e98fa46087c787c55ae2418e
bekabdimanasov <bekabdimanasov@gmail.com>
2020-05-10 08:46:01 +0300

    10. Практика. Работа с сервером - 1

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './QuizList.css'
 import {NavLink} from 'react-router-dom'
+import axios from 'axios'
 
 export default class QuizList extends Component {
 
@@ -15,6 +16,12 @@ export default class QuizList extends Component {
           </NavLink>
         </li>
       )
+    })
+  }
+
+  componentDidMount() {
+    axios.get('https://quiz-react-8c92a.firebaseio.com/quiz.json').then(response => {
+      console.log(response)
     })
   }
 
